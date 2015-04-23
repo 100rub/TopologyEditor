22d19df8bb6409165351ad0f7eadb82bcd039733
DrizztDoUrden <vasja.filippov94@gmail.com>
2015-04-23 22:57:06 +0300

    Added Element Added Contact Added XMLHelper

---
+++ src/TopEd/EventsHandling/ActionInfo.java
@@ -0,0 +1,10 @@
+package TopEd.EventsHandling;
+
+/**
+ * Created by VEF on 23.04.2015.
+ */
+public class ActionInfo
+{
+    public String ID;
+
+}
---
+++ src/TopEd/EventsHandling/EventsHandler.java
@@ -0,0 +1,11 @@
+package TopEd.EventsHandling;
+
+import java.util.Queue;
+
+/**
+ * Created by VEF on 23.04.2015.
+ */
+public class EventsHandler
+{
+    private Queue<Action> _actionsQueue;
+}
